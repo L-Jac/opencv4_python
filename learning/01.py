c6940ce74a3b1e63660ba7c3c6f5bcf1c8528777
L-jac <3137501630@qq.com>
2023-04-11 20:53:10 +0800

    学习记录 2.1

---
+++ learning/01.py
@@ -0,0 +1,9 @@
+# 第二章
+# TODO
+# numpy对图像的切割分离
+#   chapter2/Numpy_Operations.py
+
+# NumPy在对图像进行通道分离时表现良好
+#   numpy将BGR图像转为RGB图像以及使用切片和索引时间
+#   快于opencv的函数(cv.cvtColor(),cv.split())
+#   chapter2/Compare_opencv_numpy.py
