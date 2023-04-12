477759f5540cbae9362f3f3624e7faf999289136
L-jac <3137501630@qq.com>
2023-04-12 12:38:42 +0800

    学习记录 2.1

--- learning/01.py
+++ learning/01.py
@@ -1,4 +1,5 @@
 # 第二章
+import cv2 as cv
 # TODO
 # numpy对图像的切割分离
 #   chapter2/Numpy_Operations.py
@@ -7,3 +8,6 @@
 #   numpy将BGR图像转为RGB图像以及使用切片和索引时间
 #   快于opencv的函数(cv.cvtColor(),cv.split())
 #   chapter2/Compare_opencv_numpy.py
+
+# 窗口函数
+thewindow = cv.nameWindow("windowname", [cv.WINDOW_AUTOSIZE | cv.WINDOW_KEEPRATIO | cv.WINDOW_GUI_EXPANDED])
